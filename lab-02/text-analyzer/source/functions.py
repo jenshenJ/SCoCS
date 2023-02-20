aae7eced0433f2306544a14ee859e03b7f5ea18e
jenshenJ <vip.carich@gmail.com>
2023-02-20 12:38:47 +0300

    feat: complete second point of first task

--- lab-02/text-analyzer/source/functions.py
+++ lab-02/text-analyzer/source/functions.py
@@ -4,3 +4,8 @@ import collections
 def sentence_counter(text: str):
     new_text = re.sub(r'[.!?][^.!?]', r'||', text)
     return len(new_text.split('||'))
+
+def non_declarative_counter(text: str):
+    text = text + '\0'
+    pattern = r'[!?][^!?]'
+    return len(re.findall(pattern, text))
