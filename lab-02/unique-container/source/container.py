f950cafdd5aeda5260dfef2758afda7f0537db8a
jenshenJ <vip.carich@gmail.com>
2023-02-20 16:20:29 +0300

    refactor: update var names and Dockerfile

--- lab-02/unique-container/source/container.py
+++ lab-02/unique-container/source/container.py
@@ -1,26 +1,31 @@
-from os import listdir
+from os import listdir, path
 from os.path import isfile, join
+import os
 
 DATA_PATH = '../data/'
 
 class UniqueContainer:
     def __init__(self):
-        self.data = set()
-        self.user_list = [f for f in listdir(DATA_PATH) if isfile(join(DATA_PATH, f))]
+        self._data = set()
+        
+        if not path.exists('../data'):
+            os.mkdir('../data')
+
+        self._user_list = [f for f in listdir(DATA_PATH) if isfile(join(DATA_PATH, f))]
         pass
 
     
     def login(self, username: str):
-        self.username = username
+        self._username = username
         try:
-            self.file = open(join('..', 'data', self.username), 'r+')
-            self.data = set(self.file.read().split('||'))
+            self._file = open(join('..', 'data', self._username), 'r+')
+            self._data = set(self._file.read().split('||'))
         except FileNotFoundError:
-            self.file = open(join('..', 'data', self.username), 'w+') 
-            self.user_list.append(self.username)    
+            self._file = open(join('..', 'data', self._username), 'w+') 
+            self._user_list.append(self._username)    
     
     def save(self):
-         if len(self.data & set(self.file.read().split('||'))) != len(self.data):
+         if len(self._data & set(self._file.read().split('||'))) != len(self._data):
             print('You have unsaved changes. Do you want to save them? (Y/N)')
             option = ''
             options = ['y', 'Y', 'N', 'n']
@@ -28,42 +33,46 @@ class UniqueContainer:
                 option = input()
             
             if option == 'y' or option == 'Y':
-                self.file.close()
-                file_path = join('..', 'data', self.username)
-                self.file = open(file_path, 'w')
-                data_str = '||'.join(list(self.data))
-                self.file.write(data_str)
+                self._file.close()
+                file_path = join('..', 'data', self._username)
+                self._file = open(file_path, 'w')
+                data_str = '||'.join(list(self._data))
+                self._file.write(data_str)
 
         
     def load(self, filename: str):
-        file_path = join('..', 'data', self.username)
+        file_path = join('..', 'data', filename)
         if isfile(file_path):
-            self.save()
-            self.login(filename)
-            print('Container', self.username, 'has been succesfully loaded')
+            new_file = open(join('..', 'data', filename), 'r+')
+            new_data = set(new_file.read().split('||'))
+            self._data |= new_data
+            print('Container', filename, 'has been succesfully loaded')
         else:
             print("No such container")
-
-        
-
+    
     def switch(self, new_login: str):
         self.save()
         self.login(new_login)
 
     def add(self, elems: list):
         for elem in elems:
-            self.data.add(elem)
+            self._data.add(elem)
 
     def remove(self, elem: str):
-        if elem in self.data:
-            self.data.remove(elem)
+        if elem in self._data:
+            self._data.remove(elem)
             print('Element is sucsessfully delete')
         else:
             print('There is no element', elem, 'in container')
 
     def find(self, elems: list):
         for elem in elems:
-            if elem in self.data:
+            if elem in self._data:
                 print('Element ', elem, 'exists in container')
             else:
                 print('Element', elem, 'hasn\'t found')
+
+    def list(self):
+        print('Elements of container:')
+        for elem in self._data:
+            print(elem)
--- lab-02/unique-container/source/main.py
+++ lab-02/unique-container/source/main.py
@@ -3,9 +3,6 @@ from container import UniqueContainer
 container = UniqueContainer()
 
 login = input("Enter username: ")
-container.login(login)
-container.find(['123', '458', '789'])
-container.load('user2')
-container.add(['elem'])
-container.load('user3')
-container.save()
+
+
+
