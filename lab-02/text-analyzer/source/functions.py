a6599fe5a69b23c53a5a3f11d5fab362d4cd9bd0
jenshenJ <vip.carich@gmail.com>
2023-02-20 12:39:32 +0300

    feat: complete third point of first task

--- lab-02/text-analyzer/source/functions.py
+++ lab-02/text-analyzer/source/functions.py
@@ -9,3 +9,12 @@ def non_declarative_counter(text: str):
     text = text + '\0'
     pattern = r'[!?][^!?]'
     return len(re.findall(pattern, text))
+
+
+def average_chars_counter(text: str):
+    sentence_count = sentence_counter(text)
+    new_text = text.lower()                                           #ignore register
+    new_text = re.sub(r'\b[0-9]+\b\s*', '', new_text)                 #deleting from text words with numbers only
+    new_text = ''.join(ch for ch in new_text if ch.isalnum())         #deleting al non-alphabet-numeric symbols
+    return len(new_text) / sentence_count 
+
